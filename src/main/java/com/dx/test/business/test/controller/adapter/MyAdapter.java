1a129518c83667b0da99b6c44a9adad38438d782
1014049211 <50826200+1014049211@users.noreply.github.com>
2020-04-16 11:16:22 +0800

    增加注释

--- src/main/java/com/dx/test/business/test/controller/adapter/MyAdapter.java
+++ src/main/java/com/dx/test/business/test/controller/adapter/MyAdapter.java
@@ -12,8 +12,18 @@ import javax.servlet.http.HttpServletResponse;
  * 处理器适配器 HandlerAdapter
  * <p>
  * 实现 HandlerAdapter 可以自定义一个适配器
+ * <p>
+ * SpringMVC 通过 HandlerMapping 找到 Handler 以后并不是直接调用
+ * 因为 Handler 可以是任意类型, 所以需要 HandlerAdapter 这个接口来定义 Handler 的执行标准
+ * <p>
+ * SpringMVC 会在启动时将所有 HandlerAdapter 存储起来
+ * 当需要执行 Handler 时, 就遍历所有 HandlerAdapter, 通过 supports 方法判断是否可以执行这个 Handler
+ * 当 supports 返回 true 时, 就调用 handle 方法来执行 Handler
+ * SpringMVC 本身几部关心执行过程, 也不用知道执行规则, 只需提供数据并接收制定类型的返回值
+ * 这种设计不仅对 Handler 进行了解耦, 还给与开发者极大的自由
+ * 这也是接口的一个重要意义和作用
  */
-@Component
+@Component // HandlerAdapter 需要注册为 Spring 的 Bean 才能在执行 Handler 时被找到
 public class MyAdapter implements HandlerAdapter {
 
     /**
@@ -29,7 +39,9 @@ public class MyAdapter implements HandlerAdapter {
     }
 
     /**
-     * HandlerAdapter 的核心方法, 用于调用 Handler
+     * HandlerAdapter 的核心方法, 用于真正调用 Handler 来处理业务逻辑
+     * 一般来说 HandlerAdapter 作为一个介质组件, 不会进行业务逻辑的处理
+     * 主要的任务就是根据 Handler 声明的参数提供数据, 并将 Handler 返回的处理结果封装为 ModelAndView
      *
      * @param request  HttpServletRequest
      * @param response HttpServletResponse
@@ -41,11 +53,14 @@ public class MyAdapter implements HandlerAdapter {
 
         // 直接强转, 因为在 supports 方法中已经确认过类型了
         MyHandler myHandler = (MyHandler) handler;
-        // 调用 Handler 处理业务, 这里可以是任意方法或业务逻辑
-        myHandler.handle(request);
 
-        // 指定跳转的视图
-        return new ModelAndView("");
+        // 提供方法所需要的参数, 执行业务逻辑, 获得返回的视图名称
+        String viewName = myHandler.handle(request);
+
+        // 通过返回 ModelAndView 来告诉 SpringMVC 要跳转的视图和需要传递的数据
+        // ModelAndView 支持 Map 类型的数据
+        // 此处除了 Request 中已经存在的数据以外, 没有额外要传递的, 所以使用的是视图名构造方法
+        return new ModelAndView(viewName);
     }
 
     /**
