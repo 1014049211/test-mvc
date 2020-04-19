a8f275406f17c67732c8e9bc0a960955751aaece
杜骁 <1014049211@qq.com>
2020-04-19 23:03:23 +0800

    笔记类注释使用 Tips 高亮标记

--- src/main/java/com/dx/test/business/test/controller/adapter/MyAdapter.java
+++ src/main/java/com/dx/test/business/test/controller/adapter/MyAdapter.java
@@ -11,11 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * 处理器适配器 HandlerAdapter
  * <p>
- * 实现 HandlerAdapter 可以自定义一个适配器
+ * Tips 实现 HandlerAdapter 可以自定义一个适配器
  * <p>
+ * Tips 为何需要 HandlerAdapter
  * SpringMVC 通过 HandlerMapping 找到 Handler 以后并不是直接调用
  * 因为 Handler 可以是任意类型, 所以需要 HandlerAdapter 这个接口来定义 Handler 的执行标准
  * <p>
+ * Tips HandlerAdapter 如何工作
  * SpringMVC 会在启动时将所有 HandlerAdapter 存储起来
  * 当需要执行 Handler 时, 就遍历所有 HandlerAdapter, 通过 supports 方法判断是否可以执行这个 Handler
  * 当 supports 返回 true 时, 就调用 handle 方法来执行 Handler
@@ -23,13 +25,13 @@ import javax.servlet.http.HttpServletResponse;
  * 这种设计不仅对 Handler 进行了解耦, 还给与开发者极大的自由
  * 这也是接口的一个重要意义和作用
  */
-@Component // HandlerAdapter 需要注册为 Spring 的 Bean 才能在执行 Handler 时被找到
+@Component // Tips HandlerAdapter 需要注册为 Spring 的 Bean 才能在执行 Handler 时被找到
 public class MyAdapter implements HandlerAdapter {
 
     /**
      * 用于判断 HandlerAdapter 是否能处理传入的 Handler
      *
-     * @param handler 因为 Handler 可以是任意类型, 所以此处使用 Object 声明参数
+     * @param handler Tips 因为 Handler 可以是任意类型, 所以此处使用 Object 声明参数
      * @return 是否可以处理当前 Handler: true 可以, false 不可以
      */
     @Override
@@ -39,9 +41,7 @@ public class MyAdapter implements HandlerAdapter {
     }
 
     /**
-     * HandlerAdapter 的核心方法, 用于真正调用 Handler 来处理业务逻辑
-     * 一般来说 HandlerAdapter 作为一个介质组件, 不会进行业务逻辑的处理
-     * 主要的任务就是根据 Handler 声明的参数提供数据, 并将 Handler 返回的处理结果封装为 ModelAndView
+     * 调用 Handler
      *
      * @param request  HttpServletRequest
      * @param response HttpServletResponse
@@ -50,6 +50,12 @@ public class MyAdapter implements HandlerAdapter {
      */
     @Override
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        /*
+         * Tips HandlerAdapter 的核心方法, 用于真正调用 Handler 来处理业务逻辑
+         *  一般来说 HandlerAdapter 作为一个介质组件, 不会进行业务逻辑的处理
+         *  主要的任务就是根据 Handler 声明的参数提供数据, 并将 Handler 返回的处理结果封装为 ModelAndView
+         */
 
         // 直接强转, 因为在 supports 方法中已经确认过类型了
         MyHandler myHandler = (MyHandler) handler;
