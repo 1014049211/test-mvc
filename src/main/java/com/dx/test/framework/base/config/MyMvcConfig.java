65cc3db6136181a773267d1ccdf507531fe0ff31
杜骁 <1014049211@qq.com>
2020-04-19 22:54:35 +0800

    笔记类注释使用 Tips 高亮标记

--- src/main/java/com/dx/test/framework/base/config/MyMvcConfig.java
+++ src/main/java/com/dx/test/framework/base/config/MyMvcConfig.java
@@ -17,21 +17,24 @@ import java.util.List;
 /**
  * 通过实现 WebMvcConfigurer 来配置 SpringMVC
  * <p>
+ * TIps 关于 WebMvcConfigurer 接口
  * WebMvcConfigurer 虽然是一个接口, 但是所有方法都是 default 的, 实现类可以不实现任何方法
  * 这种做法是为了简化配置: 只需重写那些要定制的方法, 而不必为了设置一个而重写所有方法
  * <p>
+ * Tips 为何放弃抽象类 WebMvcConfigurerAdapter
  * 在 Spring5.0+ 之前的版本中, 配置 SpringMVC 需要继承抽象类 WebMvcConfigurerAdapter
  * 在 JDK1.8 给接口加入了 default 以后, Spring5.0+ 废弃了抽象类改为了接口
  * 所以 Spring5.0+ 至少需要 JDK1.8+ 的版本
  * <p>
+ * Tips 抽象类和有了 default 的接口
  * 接口有了 default 以后并不能完全替代抽象类, 至少 JDK1.8 中不能
  * 首先接口所有的方法都是 public 的, 所有属性都是 public static final 的
  * 这导致接口没有可以保护的私有方法, 并且没有状态, 所谓没状态可以理解为无论你何时访问这个接口的属性, 都是一样的
  * 而抽象类可以设置私有的方法和属性
  */
-@Configuration // 使用这个注解标识当前类为 Spring 的配置类
-@EnableWebMvc // 在 SpringMVC 中, 不添加此注解, 重写 WebMvcConfigurer 的方法无效(如果是 SpringBoot 项目无需此注解)
-@ComponentScan("com.dx") // 告诉 Spring 都有哪些包需要扫描
+@Configuration // Tips 使用这个注解标识当前类为 Spring 的配置类
+@EnableWebMvc // Tips 在 SpringMVC 中, 不添加此注解, 重写 WebMvcConfigurer 的方法无效(如果是 SpringBoot 项目无需此注解)
+@ComponentScan("com.dx") // Tips 告诉 Spring 都有哪些包需要扫描
 public class MyMvcConfig implements WebMvcConfigurer {
 
     /**
@@ -41,6 +44,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
 
     /**
      * 注册视图解析器 InternalResourceViewResolver, 用于解析 JSP 视图
+     * Tips InternalResourceViewResolver 是 SpringMVC 的默认配置, 但是需要显式的写出来配置一下前后缀
      *
      * @return InternalResourceViewResolver
      */
@@ -56,15 +60,18 @@ public class MyMvcConfig implements WebMvcConfigurer {
     }
 
     /**
-     * 重写 addResourceHandlers 方法来配置静态资源映射
+     * Tips 重写 addResourceHandlers 方法来配置静态资源映射
      * <p>
+     * Tips 为了要配置静态资源映射
      * 程序的静态资源(如图片、JS、CSS 等) 需要直接访问, 并不需要 Handler 处理
      * 但是 DispatchServlet 会处理所有请求, 自然也包括静态资源的请求
      * 此时需要告诉 DispatchServlet 那些是静态资源
      * <p>
+     * Tips 不使用静态资源映射如何处理静态资源
      * 设置 DispatchServlet 只处理特定后缀的请求也可以避免静态资源被拦截
      * 例如这样配置 DispatchServlet 要处理的请求: "/*.act"
      * 这样所有带 ".act" 后缀的请求才会被处理, 以 ".js"、".css" 等结尾的静态资源会直接放行
+     * 个人认为每个请求后面都拼个后缀的方式不优雅
      *
      * @param registry 静态资源映射器
      */
@@ -74,7 +81,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
     }
 
     /**
-     * 重写 addViewControllers 方法可以添加快速的视图跳转映射
+     * Tips 重写 addViewControllers 方法可以添加快速的视图跳转映射
      *
      * @param registry 快捷视图跳转注册器
      */
@@ -85,7 +92,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
     }
 
     /**
-     * 重写 addInterceptors 方法来注册拦截器
+     * Tips 重写 addInterceptors 方法来注册拦截器
      *
      * @param registry Interceptor 注册器
      */
@@ -105,7 +112,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
     }
 
     /**
-     * 重写 configureMessageConverters 方法注册自定义的解析器
+     * Tips 重写 configureMessageConverters 方法注册自定义的解析器
      *
      * @param converters 解析器注册器
      */
@@ -117,9 +124,9 @@ public class MyMvcConfig implements WebMvcConfigurer {
 
     /**
      * 用于处理 @RequestMapping 的 HandlerMapping
-     * 当配置了自定义的 HandlerMapping 以后, SpringMVC 将不再加载默认的 HandlerMapping
-     * 所以当需要使用的时候, 要自己注册
+     * Tips 当配置了自定义的 HandlerMapping 以后, SpringMVC 将不再加载默认的 HandlerMapping
      * <p>
+     * Tips SpringMVC 默认的 HandlerMapping
      * SpringMVC 默认的 HandlerMapping 配置在 DispatchServlet.properties 中
      * key 是 org.springframework.web.servlet.HandlerMapping
      *
@@ -132,9 +139,9 @@ public class MyMvcConfig implements WebMvcConfigurer {
 
     /**
      * 用于处理 @RequestMapping 的 HandlerAdapter
-     * 当配置了自定义的 HandlerAdapter 以后, SpringMVC 将不再加载默认的 HandlerAdapter
-     * 所以当需要使用的时候, 要自己注册
+     * Tips 当配置了自定义的 HandlerAdapter 以后, SpringMVC 将不再加载默认的 HandlerAdapter
      * <p>
+     * Tips SpringMVC 默认的 HandlerAdapter
      * SpringMVC 默认的 HandlerAdapter 配置在 DispatchServlet.properties 中
      * key 是 org.springframework.web.servlet.HandlerAdapter
      *
