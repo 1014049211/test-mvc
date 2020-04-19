d71997988b168ef4b4a53882533b15250837fa03
杜骁 <1014049211@qq.com>
2020-04-19 23:05:02 +0800

    笔记类注释使用 Tips 高亮标记

--- src/main/java/com/dx/test/business/test/controller/handler/MyHandler.java
+++ src/main/java/com/dx/test/business/test/controller/handler/MyHandler.java
@@ -6,16 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * 处理器 Handler
- * 这是一个自定义的 Handler, Spring 是允许任何类型的 Handler 的
- * <p>
- * Handler 功能上属于 Model 层, 主要功能是处理数据模型及业务逻辑
+ * Tips 这是一个自定义的 Handler, Spring 是允许任何类型的 Handler 的
+ * Tips Handler 功能上属于 Model 层, 主要功能是处理数据模型及业务逻辑
  */
-// 当 Bean 的 name 以 "/" 开头时, 会根据名字自动建立请求映射, URL 就是 name, Bean 就是这个 URL 的处理器
+// Tips 当 Bean 的 name 以 "/" 开头时, 会根据名字自动建立请求映射, URL 就是 name, Bean 就是这个 URL 的处理器
 @Component("/test/myHandler")
 public class MyHandler {
 
     /**
-     * 定义一个处理方法, 这个方法可以随意定义参数和返回, 只要存在可以处理此方法的 HandlerAdapter 即可
+     * Tips 定义一个处理方法, 这个方法可以随意定义参数和返回, 只要存在可以处理此方法的 HandlerAdapter 即可
      *
      * @param request HttpServletRequest
      * @return 此处返回的是视图名称
