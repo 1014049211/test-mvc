7b17bbc2f37563273e80a1e58f31a787f7d3bd35
杜骁 <1014049211@qq.com>
2020-04-19 23:06:44 +0800

    笔记类注释使用 Tips 高亮标记

--- src/main/java/com/dx/test/business/test/controller/handler/MyHttpRequestHandler.java
+++ src/main/java/com/dx/test/business/test/controller/handler/MyHttpRequestHandler.java
@@ -9,19 +9,18 @@ import java.io.IOException;
 /**
  * 处理器 Handler
  * <p>
- * 通过实现 HttpRequestHandler 接口定义一个 Handler
- * 这种 Handler 需要 HandlerMapping 的协助才能跟请求对应上
+ * Tips 通过实现 HttpRequestHandler 接口定义一个 Handler
+ * Tips 这种 Handler 需要手动添加到 HandlerMapping 中才能跟请求对应上
  */
 public class MyHttpRequestHandler implements HttpRequestHandler {
 
     /**
      * HttpRequestHandler 接口只有这一个方法
-     * 因为没有返回值,  HttpRequestHandler 类型的 Handler 并不涉及视图的查找和解析等过程
-     * 这种 Handler 适用于处理简单的 HTTP 请求, 例如一些不需要视图层的静态资源: JS, CSS, 图片等
+     * Tips 因为没有返回值,  HttpRequestHandler 类型的 Handler 并不涉及视图的查找和解析等过程
+     * Tips 这种 Handler 适用于处理简单的 HTTP 请求, 例如一些不需要视图层的静态资源: JS, CSS, 图片等
      *
      * @param request  HttpServletRequest
      * @param response HttpServletResponse
-     * @throws IOException 使用 HttpServletResponse 向页面写内容时可能会抛出此异常
      */
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
