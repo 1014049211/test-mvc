40fbc53206239800f1ba15bfdd3bae3c72e45e40
杜骁 <1014049211@qq.com>
2020-04-22 22:22:32 +0800

    增加注释: 什么是面向切面(AOP)

--- src/main/java/com/dx/test/framework/base/aop/MyAspect.java
+++ src/main/java/com/dx/test/framework/base/aop/MyAspect.java
@@ -13,6 +13,25 @@ import org.springframework.stereotype.Component;
 @Component // Tips 想让 Spring 感知到这个切面, 还要注册为 Spring 的 Bean
 public class MyAspect {
 
+    /*
+     * TIps 面向切面(AOP)
+     *  1. AOP 是一种概念, Spring 只是实现了这种概念, 并不是发明和创造了 AOP
+     *   在传统的 OOP(面向对象) 开发中, 代码的逻辑是自上而下的线性执行, 在执行过程中, 会产生横切性问题
+     *   而这些横切性问题往往与业务逻辑关系不大, AOP 就是用来剥离代码中与业务无关的横切性问题
+     *  2. 什么是横切性问题:
+     *   当需要为多个不具有继承关系的对象引入同一个公共行为时(例如日志、安全检测等)
+     *   在 OOP 的线性执行中只能在每个对象中引用这个公共行为, 产生大量的重复代码, 不便于统一维护
+     *  3. AOP 是如何解决横切性问题的:
+     *   与 OOP 中调用一个具备所需公共行为的对象来做这件事不同, AOP 是让程序的某一类行为去触发这个公共行为
+     *   就像 JS 中的响应事件一样(例如: onclick、onchange等), AOP 可以在某一类行为的开始前、结束后或发生异常时, 自动去执行指定的公共行为
+     *  4. 举例说明 Spring 中的 AOP 应用:
+     *   Spring 的事务控制是基于 AOP 的
+     *   在 Spring 中, 想让一个方法内的数据库操作保持一个事务, 只需要在方法上标注 @Transactional 注解
+     *   Spring 依据 @Transactional 注解上的属性设置自动处理事务:
+     *   方法开始前自动开始事务, 执行结束后自动提交事务, 发生异常时自动回滚事务
+     *  5. Spring 是怎么做到 AOP 的: Spring 使用代理来实现 AOP
+     */
+
     /**
      * 一个基于 execution 语法的切点
      * <p>
