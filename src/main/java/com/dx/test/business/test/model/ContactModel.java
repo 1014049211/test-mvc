135e74a030ecf6cbfe8c00488d539e45120555d5
杜骁 <1014049211@qq.com>
2020-04-27 21:46:53 +0800

    联系方式实体类

---
+++ src/main/java/com/dx/test/business/test/model/ContactModel.java
@@ -0,0 +1,21 @@
+package com.dx.test.business.test.model;
+
+import lombok.Data;
+
+/**
+ * 联系方式实体类
+ */
+@Data
+public class ContactModel {
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+}
